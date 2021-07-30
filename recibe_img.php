739e81b912b35163c7cdce5d01648d61d16ecbb8
URIAN VIERA <urian1213viera@gmail.com>
2021-07-29 22:11:52 -0500

    Add files via upload
    Registrando imagen en bd

--- recibe_img.php
+++ recibe_img.php
@@ -1,14 +1,24 @@
 <?php
-//include('config.php');
+/* Conexion a BD */
+/*$usuario  = "root";
+$password = "";
+$servidor = "localhost";
+$basededatos = "practicas";
+$con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
+$db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
+*/
 
 
-//Recibo la foto (Factura)
-$filename       = $_FILES['foto']['name'];
-$sourceFoto     = $_FILES['foto']['tmp_name'];
+//$nombreCliente   = $_REQUEST['nombreCliente'];
+$filename        = $_FILES['imagenCliente']['name'];
+$sourceFoto      = $_FILES['imagenCliente']['tmp_name'];
 
+date_default_timezone_set("America/Bogota");
+setlocale(LC_ALL,"es_ES");
+$fecha_imagen   = date("d/m/Y h:i A");
 
-$logitudPass  = 10;
-$newNameFoto   = substr( md5(microtime()), 1, $logitudPass);
+$logitudPass    = 10;
+$newNameFoto    = substr( md5(microtime()), 1, $logitudPass);
 
 $explode        = explode('.', $filename);
 $extension_foto = array_pop($explode);
@@ -20,37 +30,27 @@ if (!file_exists($dirLocal)) {
     mkdir($dirLocal, 0777, true);
 }
 
-$miDir    = opendir($dirLocal); //Habro el directorio
-$urlFactura = $dirLocal.'/'.$nuevoNameFoto;
+$miDir         = opendir($dirLocal); //Habro el directorio
+$imagenCliente = $dirLocal.'/'.$nuevoNameFoto;
 
-if(move_uploaded_file($sourceFoto, $urlFactura)){
-/*  
+if(move_uploaded_file($sourceFoto, $imagenCliente)){
+ /*
   $sqlInsertBD = ("
-  INSERT INTO clientesGroomers (
-      nameFull,
-      typeMascot,
-      year,
-      phone,
-      email,
-      groomer_id,
-      photo_invoice,
-      fecha_registro
+  INSERT INTO imagenes (
+      nombreCliente,
+      imagenCliente,
+      fecha_imagen
     )
   VALUES (
-    '$nameFull',
-    '$typeMascot',
-    '$year',
-    '$phone',
-    '$email',
-    '$idgroomer',
-    '$urlFactura',
-    '$fecha_registro'
+    '$nombreCliente',
+    '$imagenCliente',
+    '$fecha_imagen'
   );");
   $post_data_query = mysqli_query($con, $sqlInsertBD);
-  */
-}
 
+*/
+}
 //@mysqli_close($con); 
 
-
+header("Location:index.php");
 ?>
